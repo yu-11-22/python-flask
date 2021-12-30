adbeb5cb15548bc11d6449ac8651ed2362454e02
willy24692485 <willy24692485@gmail.com>
2021-12-30 22:58:05 +0800

    make it better

--- index.py
+++ index.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 def hello_world():
     return render_template("index.html")
 
-if __name__ == "__main__":
+if __name__ == "__main__he":
     app.run(host = "0.0.0.0", port = 8000, debug = True)
